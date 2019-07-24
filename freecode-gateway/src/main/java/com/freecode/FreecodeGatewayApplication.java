563abe1ee96716908bd5050dd9d025d66333cebb
lewesyang <lewesyang@aliyun.com>
2019-07-24 16:43:48 +0800

    add readme content

---
+++ freecode-gateway/src/main/java/com/freecode/FreecodeGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.freecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreecodeGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreecodeGatewayApplication.class, args);
+	}
+
+}
---
+++ freecode-gateway/src/test/java/com/freecode/FreecodeGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.freecode;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FreecodeGatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
