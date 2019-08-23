68f095cf05b7e799e316f5ef7fa4ef7a483af0c3
lewesyang <lewesyang@aliyun.com>
2019-08-23 16:01:40 +0800

    tolowercase

--- freecode-gateway/src/main/java/com/freecode/FreecodeGatewayApplication.java
+++ freecode-gateway/src/main/java/com/freecode/FreecodeGatewayApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @EnableAutoConfiguration
 public class FreecodeGatewayApplication {
 
