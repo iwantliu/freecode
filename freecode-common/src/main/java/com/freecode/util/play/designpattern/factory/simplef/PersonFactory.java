2195d4116d450f77950930d1ef82f2eaf4b935ca
lewesyang <lewesyang@aliyun.com>
2019-11-29 10:01:50 +0800

    thinker

--- freecode-common/src/main/java/com/freecode/util/play/designpattern/factory/simplef/PersonFactory.java
+++ freecode-common/src/main/java/com/freecode/util/play/designpattern/factory/simplef/PersonFactory.java
@@ -1,12 +1,6 @@
 package com.freecode.util.play.designpattern.factory.simplef;
 
-/**
- * <p>Description:</p>
- *
- * @author stliu
- * @version 1.0
- * @createtime 2019-11-28 20:19
- */
+
 public class PersonFactory {
 
     private static Person createPerson(String type) {
